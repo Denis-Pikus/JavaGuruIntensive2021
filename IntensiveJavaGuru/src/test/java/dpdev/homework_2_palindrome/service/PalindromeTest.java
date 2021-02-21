94763d7f73abe51294328efac423a17d97a729ce
Denis-Pikus <amom@inbox.ru>
2021-02-21 23:28:29 +0300

    I have created test for method swap()

--- IntensiveJavaGuru/src/test/java/dpdev/homework_2_palindrome/service/PalindromeTest.java
+++ IntensiveJavaGuru/src/test/java/dpdev/homework_2_palindrome/service/PalindromeTest.java
@@ -1,12 +1,15 @@
 package dpdev.homework_2_palindrome.service;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 import org.junit.Assert;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class PalindromeTest extends TestCase {
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class PalindromeTest {
     Logger logger = LoggerFactory.getLogger(PalindromeTest.class);
     Palindrome palindrome = new Palindrome();
 
@@ -16,19 +19,19 @@ public class PalindromeTest extends TestCase {
         String testString = "11,?2  1.1/ ";
         logger.info("Test string {}", testString);
         char[] actual = palindrome.deleteNotLettersAndDigitSymbols(testString);
-        Assert.assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, actual);
         logger.info("Modified string {}", actual.toString());
     }
 
-//    @Test
-//    void swap() {
-//        int[] testArray = {2, 0, 1, 5};
-//        int[] expected = {5, 1, 0, 2};
-//        logger.info("Original array is: {}", Arrays.toString(testArray));
-//        arrayService.swap(testArray);
-//        assertArrayEquals(expected, testArray);
-//        logger.info("Swapped array is: {}", Arrays.toString(testArray));
-//    }
+    @Test
+    public void swap() {
+        char[] testArray = "123".toCharArray();
+        char[] expected = "321".toCharArray();
+        logger.info("Original array is: {}", Arrays.toString(testArray));
+        palindrome.swap(testArray);
+        assertArrayEquals(expected, testArray);
+        logger.info("Swapped array is: {}", Arrays.toString(testArray));
+    }
 
     @Test
     public void testIsPalindrome() {

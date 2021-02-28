e82077d358ea69546ed273ec2a453dd9c6171def
Denis-Pikus <amom@inbox.ru>
2021-02-28 22:19:06 +0300

    I completed class for (Map.Entry<String, Integer> entry: map.entrySet()) {

--- IntensiveJavaGuru/src/main/java/dpdev/Unique_word_counter/UniqueWordCounter.java
+++ IntensiveJavaGuru/src/main/java/dpdev/Unique_word_counter/UniqueWordCounter.java
@@ -1,4 +1,30 @@
 package dpdev.Unique_word_counter;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class UniqueWordCounter {
+    protected Map<String, Integer> map = new HashMap<>();
+
+    public void addWord(String word){
+        if (map.containsKey(word)){
+            map.put(word, map.get(word) + 1);
+        }
+        else map.put(word, 1);
+    }
+
+    public String getMostFrequentWord(){
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            if (entry.getValue() == Collections.max(map.values()))
+                return entry.getKey();
+        }
+        return "";
+    }
+
+    public void printWordsFrequency(){
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            System.out.println(entry.getValue() + " - " + entry.getKey());
+        }
+    }
 }
---
+++ IntensiveJavaGuru/src/test/java/dpdev/Unique_word_counter/UniqueWordCounterTest.java
@@ -0,0 +1,8 @@
+package dpdev.Unique_word_counter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UniqueWordCounterTest {
+    UniqueWordCounter wordCounter = new UniqueWordCounter();
+
+}

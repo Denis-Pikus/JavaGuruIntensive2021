a77186d26c56f0540b6239f373c0a853f8cd7e52
Denis-Pikus <amom@inbox.ru>
2021-04-05 22:51:50 +0300

    I have refactored the task

--- IntensiveJavaGuru/src/main/java/dpdev/pascal_triangle/Bean/Diagonal.java
+++ IntensiveJavaGuru/src/main/java/dpdev/pascal_triangle/Bean/Diagonal.java
@@ -1,42 +1,39 @@
 package dpdev.pascal_triangle.Bean;
 
-import java.math.BigInteger;
-
 public class Diagonal {
-    private BigInteger[][] pascalTriangle;
 
-    public Diagonal() {
-
-    }
-
-    public BigInteger diagonal(int n, int p) {
-        BigInteger sum = BigInteger.ZERO;
+    public static int diagonal(int n, int p) {
+        int[][] array = createArray(n);
+        show(array);
         if (n > 0 && p > 0 && n > p) {
-            pascalTriangle = pascalArray(n);
-            show();
+            int sum = 0;
+            for (int i = 0; i < (n - p + 1); i++) {
+                sum += array[i][p];
+            }
+            return sum;
         } else throw new IllegalArgumentException("Bad arguments!");
-        for (int i = 0; i < n - p + 1; i++) {
-            sum = sum.add(pascalTriangle[i][p]);
-        }
-        return sum;
     }
 
-    private BigInteger[][] pascalArray(int n) {
-        pascalTriangle = new BigInteger[n + 1][n + 1];
-        for (int i = 0; i <= n; i++) {
-            this.pascalTriangle[i][0] = this.pascalTriangle[i][i] = BigInteger.ONE;
-            for (int j = 1; j < i; j++) {
-                this.pascalTriangle[i][j] =
-                        this.pascalTriangle[i - 1][j - 1].add(this.pascalTriangle[i - 1][j]);
+    private static int[][] createArray(int n) {
+        int[][] array = new int[n + 1][n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            array[i][0] = 1;
+        }
+        for (int j = 0; j < n + 1; j++) {
+            array[0][j] = 1;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                array[i][j] = array[i][j - 1] + array[i - 1][j];
             }
         }
-        return this.pascalTriangle;
+        return array;
     }
 
-    public void show() {
-        for (int i = 0; i < this.pascalTriangle.length; i++) {
+    public static void show(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < i; j++) {
-                System.out.print(this.pascalTriangle[i][j] + " ");
+                System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
--- IntensiveJavaGuru/src/main/java/dpdev/pascal_triangle/Main.java
+++ IntensiveJavaGuru/src/main/java/dpdev/pascal_triangle/Main.java
@@ -1,13 +1,8 @@
 package dpdev.pascal_triangle;
 
 import dpdev.pascal_triangle.Bean.Diagonal;
-
-import java.math.BigInteger;
-
 public class Main {
     public static void main(String[] args) {
-        Diagonal diagonal = new Diagonal();
-        BigInteger diagonal1 = diagonal.diagonal(9, 7);
-        System.out.println(diagonal1);
+        System.out.println(Diagonal.diagonal(20, 4));
     }
 }
